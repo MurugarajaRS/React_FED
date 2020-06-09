27c854185eaafe043d96c71aa520098a167c38e9
MurugarajaRS <hutsraja@gmail.com>
2020-06-09 17:41:39 +0530

    Updates

---
+++ precache-manifest.2496c3e943effe25b5f62522ef6e9d76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ff991a02fa5682f0b8cc9791487e59",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "625eddcaed9731459ed3",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b0f33d25884d6e65aa2",
+    "url": "/react/static/js/2.928f7d8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.928f7d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625eddcaed9731459ed3",
+    "url": "/react/static/js/main.9c931e9f.chunk.js"
+  },
+  {
+    "revision": "c490829ee478042c14b6",
+    "url": "/react/static/js/runtime-main.4abd8630.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
